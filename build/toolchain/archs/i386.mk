af0734da97447a95d757c693d55cb34180217d3a
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-02-07 22:55:47 +0100

    arch-i386: started porting to ia-32.

---
+++ build/toolchain/archs/i386.mk
@@ -0,0 +1,21 @@
+
+ARCH_KCFLAGS = \
+	-fno-stack-protector \
+	-mno-80387 \
+	-mno-mmx \
+	-mno-3dnow \
+	-mno-sse \
+	-mno-sse2 \
+	-mno-red-zone \
+	-mcmodel=kernel
+
+ARCH_CFLAGS = \
+
+USER_AS=nasm
+USER_ASFLAGS=-f elf64
+
+QEMU_ARGS= \
+		-M q35 \
+		-smp 4 \
+		-m 256M \
+		-rtc base=localtime
